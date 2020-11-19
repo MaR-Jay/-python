a828a5786e455fc351050b56a9d6c9a9c04cbc33
MaR-Jay <65756677+MaR-Jay@users.noreply.github.com>
2020-11-19 10:59:32 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 base_url = ''
 
-
+# 伪装浏览器请求
 def get_url(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/86.0.4240.198 Safari/537.36'}
@@ -13,7 +13,7 @@ def get_url(url):
     bsobj = BeautifulSoup(resp.text, 'html.parser')
     return bsobj
 
-
+# 获取连接，返回
 def get_pages():
     pages_ = []
     bs = get_url('')
@@ -24,7 +24,7 @@ def get_pages():
         pages_.append((title, base_url + href))
     return pages_
 
-
+# 解析数据
 def get_content(link=None):
     if not link:
         bsobj = get_url(url)
@@ -36,7 +36,7 @@ def get_content(link=None):
     text = content[0].text.replace('\xa0' * 8, '\n\n')
     return title, text
 
-
+# 写入文件
 def write_text(text):
     with open('flie/%s.txt' % text[0], 'w', encoding='utf8') as f:
         for line in text:
